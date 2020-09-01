4ced480d98fd1eb75ba36317bad0eacecb58706a
alphaka <63592880+alphaka@users.noreply.github.com>
2020-09-01 13:16:43 -0400

    Update averageOfNum.java
    Third Commit

--- averageOfNum.java
+++ averageOfNum.java
@@ -3,7 +3,7 @@ public class averageOfNum {
 	public static void main(String[] args) {
 
 		int sum =0;
-
+		// Created a sum variable which will hold the sum of all integers
 		for(int i=0; i<args.length; i++)
 			sum+=(Integer.parseInt(args[i]));
 		
