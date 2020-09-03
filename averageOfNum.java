a39179c0e9c4d9c3caa9be395bc89c1786653377
alphaka <63592880+alphaka@users.noreply.github.com>
2020-09-03 08:47:10 -0400

    Update averageOfNum.java

--- averageOfNum.java
+++ averageOfNum.java
@@ -6,8 +6,6 @@ public class averageOfNum {
 		// Created a sum variable which will hold the sum of all integers
 		for(int i=0; i<args.length; i++)
 			sum+=(Integer.parseInt(args[i]));
-		
-		int size=args.length;
 
 		System.out.println("The average of the numbers is: " + 
 				   sum/args.length);
